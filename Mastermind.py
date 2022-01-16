a4a0a9e033db6e03cce59a6163a6e18276e92461
Foxtrot-813 <84441705+Foxtrot-813@users.noreply.github.com>
2022-01-16 04:39:08 +0100

    Update Mastermind.py

--- Mastermind.py
+++ Mastermind.py
@@ -1,19 +1,38 @@
 import random
 import time
+import sys
 
-choice = input("Please choose the difficulty of the game.\nEasy \nNormal \nHard\n")
+s = "★━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━★"
+choice = input("Please choose the difficulty of the game.\n1- Easy \n2- Normal \n3- Hard\n4- Quite\n")
+shots = 0
+
+try:
+    if choice.lower() == 'Quite' or choice == '4':
+        sys.exit()
+    elif choice.lower() == 'easy' or choice == '1':
+        shots = 10
+    elif choice.lower() == 'normal' or choice == '2':
+        shots = 7
+    elif choice.lower() == 'hard' or choice == '3':
+        shots = 5
+    else:
+        print("Invalid number.")
+except ValueError:
+    print(f"{s}\nError, invalid value.\n{s}")
 
 
 def level(choice):
-    if choice.lower() == 'easy':
-        x = random.sample(range(0, 10), 3)
-    elif choice.lower() == 'normal':
-        x = random.sample(range(0, 10), 4)
-    elif choice.lower() == 'hard':
-        x = random.sample(range(0, 10), 5)
+    if choice.lower() == 'Quite' or choice == '4':
+        sys.exit()
+    elif choice.lower() == 'easy' or choice == '1':
+        difficulty = random.sample(range(0, 10), 3)
+    elif choice.lower() == 'normal' or choice == '2':
+        difficulty = random.sample(range(0, 10), 4)
+    elif choice.lower() == 'hard' or choice == '3':
+        difficulty = random.sample(range(0, 10), 5)
     else:
         print("Invalid number.")
-    return x
+    return difficulty
 
 
 secret_code = level(choice)
@@ -21,7 +40,7 @@ print(f"This is the secret code, you can turn off this function if you want. {se
 
 attempts = 1
 
-while attempts <= 5:
+while attempts <= shots:
     guess = input("Please input your numbers:\n").split()
     matches = 0
     hits = 0
@@ -47,9 +66,11 @@ while attempts <= 5:
 
     # Using map() to perform conversion, converting all srt items in guess list to int.
     if list(map(int, guess)) == secret_code:
-        print("Congratulations, you have managed to guess all numbers in the secret code.")
+        print("★ Congratulations, you have managed to guess all numbers in the secret code ★")
         break
     attempts += 1
     print(f"{guess}    Hits =  {hits}    Matches = {matches}")
-    if attempts == 6:
-        print("Game over")
+    if attempts > shots:
+        print(f"{s}\nGame Over.\n{s}")
+    elif shots - attempts == 0:
+        print(f"{s}\nLast Chance.\n{s}")
